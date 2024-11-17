8fb053b184a021f990f2b5ca6e9ab8e0f16bcd3b
stevejay675 <stevejay675@gmail.com>
2024-11-17 19:06:56 +0100

    rigt nav  component initial  commit

---
+++ src/components/atoms/message.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../molecules/right_nav/rightNav.scss'
+
+const Message = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Message
---
+++ src/components/molecules/right_nav/rightNav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './rightNav.scss';
+import Message from '../../atoms/message';
+
+const rightNav = () => {
+  return (
+    <div>
+      <Message />
+    </div>
+  )
+}
+
+export default rightNav
