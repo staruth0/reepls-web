0cea3a4e8934b7e4af8d6d3a9abdb1eeaa05772e
glennfor <glennfor72@gmail.com>
2024-12-21 06:53:40 -0500

    fix: add unstyled loading state for editor

--- src/feature/Blog/components/EditorJS.tsx
+++ src/feature/Blog/components/EditorJS.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState, useRef } from "react";
-import EditorJS from "@editorjs/editorjs";
-import Header from "@editorjs/header";
-import List from "@editorjs/list";
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Editor: React.FC = () => {
   const editorRef = useRef<HTMLDivElement | null>(null);
   const [editor, setEditor] = useState<EditorJS | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (!editorRef.current) return;
-    console.log(editor)
+    console.log(editor);
 
     const editorInstance = new EditorJS({
       holder: editorRef.current, // Attach to the ref instead of ID
@@ -17,15 +18,17 @@ const Editor: React.FC = () => {
         header: Header,
         list: List,
       },
-      placeholder: "Start writing your content here...",
+      placeholder: 'Start writing your content here...',
     });
 
     editorInstance.isReady
       .then(() => {
         setEditor(editorInstance);
+        setIsLoading(false);
       })
       .catch((error) => {
-        console.error("Error initializing EditorJS:", error);
+        console.error('Error initializing EditorJS:', error);
+        setIsLoading(false);
       });
 
     return () => {
@@ -34,20 +37,12 @@ const Editor: React.FC = () => {
           editorInstance.destroy();
         })
         .catch((error) => {
-          console.error("Error during cleanup:", error);
+          console.error('Error during cleanup:', error);
         });
     };
   }, []);
 
-  return (
-    <>
-      <div
-        ref={editorRef}           
-              id="editorjs"
-          ></div>
-          
-    </>
-  );
+  return <>{isLoading ? <div>Loading editor...</div> : <div ref={editorRef} id="editorjs"></div>}</>;
 };
 
 export default Editor;
