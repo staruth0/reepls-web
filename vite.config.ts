b5d5a985402328ae2862b766ef4b6e182b53b7eb
glennfor <glennfor72@gmail.com>
2024-12-21 06:08:18 -0500

    fix: remove SASS legacy compiler warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+/*
+Find out here why change from legacy to modern: https://vite.dev/config/shared-options.html#css-preprocessoroptions
+*/
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler', // or "modern", "legacy"
+        importers: [
+          // ...
+        ],
+      },
+    },
+  },
+});
