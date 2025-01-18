a6770ae8cd120eae6f911bf4f6f45e7b31251696
glennfor <glennfor72@gmail.com>
2025-01-18 15:14:58 -0500

    fix: convert img to icons

--- src/components/atoms/BlogComponents/BlogReactionSession.tsx
+++ src/components/atoms/BlogComponents/BlogReactionSession.tsx
@@ -1,32 +1,39 @@
-import React, { useContext} from "react";
-
-import "./Blog.scss";
-import { readAloudIcon, thumb_2, tooltip } from "../../../assets/icons";
-import { VoiceLanguageContext } from "../../../context/VoiceLanguageContext/VoiceLanguageContext";
+import React, { useContext, useState } from 'react';
+import { LuAudioLines, LuMessageCircle, LuThumbsUp, LuVolumeX } from 'react-icons/lu';
+import { VoiceLanguageContext } from '../../../context/VoiceLanguageContext/VoiceLanguageContext';
+import { cn } from '../../../utils';
 
 const BlogReactionSession: React.FC = () => {
   const { selectedVoice } = useContext(VoiceLanguageContext);
+  const [isSpeaking, setIsSpeaking] = useState<boolean>(false);
 
   const handleSpeak = () => {
-    const utterance = new SpeechSynthesisUtterance(
-      `The Tangue", alongside a number of other historic artworks from the fatherland have not found home yet. These artifacts remain scattered across various countries, awaiting their rightful return to their homeland.`
-    );
-    utterance.voice = selectedVoice;
-    console.log(selectedVoice);
+    const message = `The Tangue", alongside a number of other historic artworks from the fatherland have not found home yet. These artifacts remain scattered across various countries, awaiting their rightful return to their homeland.`;
+    setIsSpeaking(true);
+    const utterance = new SpeechSynthesisUtterance(message);
+    if (selectedVoice) utterance.voice = selectedVoice;
     window.speechSynthesis.speak(utterance);
+    setIsSpeaking(false);
+  };
 
-  }
+  const cancelSpeaking = () => {
+    window.speechSynthesis.cancel();
+    setIsSpeaking(false);
+  };
 
   return (
     <div className="blog-reaction-session">
-      <button>
-        <img src={thumb_2} alt="thumb" /> React
+      <button className="hover:text-primary-400 cursor-pointer">
+        <LuThumbsUp className="size-5" /> React
       </button>
-      <button>
-        <img src={tooltip} alt="" /> Comment
+      <button className="hover:text-primary-400 cursor-pointer">
+        <LuMessageCircle className="size-5" /> Comment
       </button>
-      <button onClick={handleSpeak}>
-        <img src={readAloudIcon} alt="" /> Read Aloud
+      <button
+        className={cn('hover:text-primary-400 cursor-pointer', isSpeaking && 'animate-pulse')}
+        onClick={isSpeaking ? cancelSpeaking : handleSpeak}>
+        {isSpeaking ? <LuVolumeX className="size-5 animate-spin" /> : <LuAudioLines className="size-5" />}
+        {isSpeaking ? 'Stop' : 'Read Aloud'}
       </button>
     </div>
   );
