17799dfa5e7af529e935fb6a19c32710f18dee00
stevejay675 <stevejay675@gmail.com>
2025-01-19 15:21:53 +0100

    updates

--- src/components/atoms/BlogComponents/BlogReactionSession.tsx
+++ src/components/atoms/BlogComponents/BlogReactionSession.tsx
@@ -1,25 +1,28 @@
-import React, { useContext} from "react";
-
+import React, { useContext, useState } from "react";
 import "./Blog.scss";
-import { readAloudIcon, thumb_2, tooltip } from "../../../assets/icons";
+import { thumb_2, tooltip, readAloudIcon } from "../../../assets/icons";
 import { VoiceLanguageContext } from "../../../context/VoiceLanguageContext/VoiceLanguageContext";
+import ReactionModal from "./ReactionModal";
 
 const BlogReactionSession: React.FC = () => {
   const { selectedVoice } = useContext(VoiceLanguageContext);
+  const [isModalOpen, setModalOpen] = useState(false);
 
   const handleSpeak = () => {
     const utterance = new SpeechSynthesisUtterance(
-      `The Tangue", alongside a number of other historic artworks from the fatherland have not found home yet. These artifacts remain scattered across various countries, awaiting their rightful return to their homeland.`
+      `The Tangue, alongside a number of other historic artworks from the fatherland, have not found home yet. These artifacts remain scattered across various countries, awaiting their rightful return to their homeland.`
     );
     utterance.voice = selectedVoice;
-    console.log(selectedVoice);
     window.speechSynthesis.speak(utterance);
-
-  }
+  };
 
   return (
     <div className="blog-reaction-session">
-      <button>
+      <button
+        onMouseEnter={() => setModalOpen(true)}
+        onClick={() => setModalOpen(true)}
+        className="flex items-center gap-2"
+      >
         <img src={thumb_2} alt="thumb" /> React
       </button>
       <button>
@@ -28,6 +31,13 @@ const BlogReactionSession: React.FC = () => {
       <button onClick={handleSpeak}>
         <img src={readAloudIcon} alt="" /> Read Aloud
       </button>
+
+      
+      <ReactionModal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        onReact={(reaction) => console.log(`Reacted with ${reaction}`)}
+      />
     </div>
   );
 };
--- src/components/atoms/BlogComponents/BlogReactionStats.tsx
+++ src/components/atoms/BlogComponents/BlogReactionStats.tsx
@@ -1,21 +1,23 @@
-
 import React from "react";
 import "./Blog.scss";
 import { hand5, heart, thumb } from "../../../assets/icons";
+
 
 const BlogReactionStats: React.FC = () => {
   return (
     <div className="blog-reaction-stats">
       <div className="icons">
-        <div>
+        <div className="icons-count-wrapper">
+          <div className="icon-wrapper">
           <img src={heart} alt="heart" />
           <img src={thumb} alt="thumb" />
           <img src={hand5} alt="hand" />
         </div>
-        <div>
-          1.1 k
+        <div className="reaction-count">
+          1.1k
         </div>
-        <div>
+        </div>
+        <div className="comment-count">
           57 Comments
         </div>
       </div>
