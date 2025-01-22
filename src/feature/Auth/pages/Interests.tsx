c8404eb00fbb6b759594bf7616881a449652d349
glennfor <glennfor72@gmail.com>
2025-01-22 00:13:07 -0500

    fix: clean up

--- src/feature/Auth/pages/Interests.tsx
+++ src/feature/Auth/pages/Interests.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { interests } from '../../../data';
 import { RootState } from '../../../store';
+import { cn } from '../../../utils';
 import { useUpdateUser } from '../hooks/AuthHooks';
 
 function Interests() {
@@ -21,6 +22,11 @@ function Interests() {
   };
 
   const handleComplete = () => {
+    if (interest.length === 0) {
+      // toast.error('Please select at least one interest');
+      window.alert('Please select at least one interest');
+      return;
+    }
     updateUser.mutate({ interests: interest, username });
   };
 
@@ -30,7 +36,7 @@ function Interests() {
         <div>{t('Enow, you’re in! Select your interests')}</div>
         <p>{t('Last! Pick at least one topic that you are interested in')}</p>
       </div>
-      <div className={`counter__interests ${interest.length > 0 ? 'green' : null}`}>{interest.length} Selected</div>
+      <div className={cn(`counter__interests`, { green: interest.length > 0 })}>{interest.length} Selected</div>
       <div className="interest__wrapper">
         {interests.map((interest) => (
           <Interestbtn key={interest} interest={interest} handleInterest={handleInterest} />
@@ -40,9 +46,11 @@ function Interests() {
 
       <div className="interest_btns">
         {updateUser.isPending ? (
-          <button className={`${interest.length > 0 ? 'green' : null}`}>Completing Account Creation.......</button>
+          <button className={cn(`interest_btn`, { green: interest.length > 0 })}>
+            Completing Account Creation.......
+          </button>
         ) : (
-          <button className={`${interest.length > 0 ? 'green' : null}`} onClick={handleComplete}>
+          <button className={cn(`interest_btn`, { green: interest.length > 0 })} onClick={handleComplete}>
             {interest.length > 0 ? 'Done' : 'Sign up'}
           </button>
         )}
