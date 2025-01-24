c75c48e16a0c75ac2c707a9d3519763f6046ae40
glennfor <glennfor72@gmail.com>
2025-01-24 07:22:25 -0500

    fix: extract post modal to own component

--- src/components/molecules/sidebar/Sidebar.tsx
+++ src/components/molecules/sidebar/Sidebar.tsx
@@ -1,14 +1,4 @@
-import {
-  Button,
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  DialogTitle,
-  Popover,
-  PopoverButton,
-  PopoverPanel,
-} from '@headlessui/react';
-import Picker, { Theme } from 'emoji-picker-react';
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 
 // import * as RadixPopover from '@radix-ui/react-popover';
 import React, { useContext, useState } from 'react';
@@ -16,28 +6,21 @@ import { FaRegUserCircle } from 'react-icons/fa';
 import {
   LuBell,
   LuBookmark,
-  LuCalendar,
   LuCircleChevronLeft,
   LuCirclePlus,
-  LuClock,
   LuHouse,
-  LuImage,
   LuPencilLine,
   LuPlus,
   LuSearch,
-  LuSmile,
-  LuVideo,
-  LuX,
 } from 'react-icons/lu';
 import { useNavigate } from 'react-router-dom';
 import SidebarItem from '../../atoms/SidebarItem';
 
 import { useTranslation } from 'react-i18next';
-import { allowedImageTypes, allowedVideoTypes } from '../../../constants';
 // import { AuthContext } from '../../../context/AuthContext/authContext';
 import { SidebarContext } from '../../../context/SidebarContext/SidebarContext';
-import useTheme from '../../../hooks/useTheme';
 import { cn } from '../../../utils';
+import PostModal from '../PostModal';
 import './sidebar.scss';
 
 interface SidebarProps {
@@ -85,6 +68,12 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarCollapsed }) => {
     },
   ];
 
+  const handlePost = (postContent: string, postImages: File[], postVideos: File[]) => {
+    console.log('postContent', postContent);
+    console.log('postImages', postImages);
+    console.log('postVideos', postVideos);
+  };
+
   // fixed top-0 h-screen w-[65%] bg-white z-10
 
   return (
@@ -116,7 +105,9 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarCollapsed }) => {
         ))}
       </div>
 
-      {isCreatingPost && <CreateRegularPostModal isModalOpen={isCreatingPost} setIsModalOpen={setIsCreatingPost} />}
+      {isCreatingPost && (
+        <PostModal isModalOpen={isCreatingPost} setIsModalOpen={setIsCreatingPost} handlePost={handlePost} />
+      )}
       <div className="create__post__btn">
         <Popover className="relative">
           <PopoverButton
@@ -181,179 +172,6 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarCollapsed }) => {
         </RadixPopover.Root>
       </div> */}
     </div>
-  );
-};
-
-const CreateRegularPostModal = ({
-  isModalOpen,
-  setIsModalOpen,
-}: {
-  isModalOpen: boolean;
-  setIsModalOpen: (value: boolean) => void;
-}) => {
-  const [postContent, setPostContent] = useState<string>('');
-  const [postImages, setPostImages] = useState<File[]>([]);
-  const [postVideos, setPostVideos] = useState<File[]>([]);
-  // const [postEvents, setPostEvents] = useState<File[]>([]);
-  // const [postOther, setPostOther] = useState<File[]>([]);
-  const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState<boolean>(false);
-  const { theme } = useTheme();
-  const { t } = useTranslation();
-
-  const onPickImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (!files) return;
-    let newImages = [];
-    for (const file of files) {
-      if (allowedImageTypes.includes(file.type)) {
-        newImages.push(file);
-      }
-    }
-    setPostImages([...postImages, ...newImages]);
-  };
-
-  const onPickVideo = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    console.log('picking video', files);
-    if (!files) return;
-    let newVideos = [];
-    console.log('picking ');
-    for (const file of files) {
-      console.log('file', file.type);
-      if (allowedVideoTypes.includes(file.type)) {
-        newVideos.push(file);
-      }
-    }
-    setPostVideos([...postVideos, ...newVideos]);
-    console.log('picked video', newVideos);
-  };
-
-  return (
-    <Dialog
-      open={isModalOpen}
-      as="div"
-      className="relative z-10 focus:outline-none"
-      onClose={() => setIsModalOpen(false)}>
-      <DialogBackdrop className="fixed inset-0 bg-black/30" />
-
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center p-4">
-          <DialogPanel
-            transition
-            className={cn(
-              'w-full h-full  md:max-w-xl lg:max-w-2xl xl:max-w-3xl  rounded-xl bg-background p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0',
-              isModalOpen ? 'opacity-100' : 'opacity-0'
-            )}>
-            <DialogTitle as="h3" className="text-base/7 font-medium mb-4 flex-1">
-              <div className="flex justify-between items-center">
-                <div className="text-lg font-semibold">Post to anyone</div>
-                <button onClick={() => setIsModalOpen(false)}>
-                  <LuX className="size-6" />
-                </button>
-              </div>
-            </DialogTitle>
-            <div className=" h-[100%] flex-grow">
-              <textarea
-                className="w-full p-2 mb-4 border-none outline-none h-80 bg-background"
-                autoFocus
-                placeholder="What's on your mind?"
-                rows={15}
-                value={postContent}
-                onChange={(e) => setPostContent(e.target.value)}
-              />
-              {(postImages.length > 0 || postVideos.length > 0) && (
-                <div className="display-media flex justify-start items-center overflow-x-auto gap-2 my-1 py-1 px-4 border-b border-t border-neutral-400">
-                  {postImages.map((image, index) => (
-                    <div key={image.name} className="relative block h-32 w-32 aspect-w-1 aspect-h-1 flex-shrink-0">
-                      <img src={URL.createObjectURL(image)} alt="post image" className="object-cover h-full w-auto" />
-                      <button
-                        className="absolute top-0 right-0 bg-red-500 text-white rounded-full p-1"
-                        onClick={() => {
-                          const updatedImages = postImages.filter((_, i) => i !== index);
-                          setPostImages(updatedImages);
-                        }}>
-                        <LuX className="size-3" />
-                      </button>
-                    </div>
-                  ))}
-                  {postVideos.map((video, index) => (
-                    <div key={video.name} className="relative w-32 h-32 flex-shrink-0">
-                      <video src={URL.createObjectURL(video)} className="object-cover w-auto h-full" />
-                      <button
-                        className="absolute top-0 right-0 bg-red-500 text-white rounded-full p-1"
-                        onClick={() => {
-                          const updatedVideos = postVideos.filter((_, i) => i !== index);
-                          setPostVideos(updatedVideos);
-                        }}>
-                        <LuX className="size-3" />
-                      </button>
-                    </div>
-                  ))}
-                </div>
-              )}
-
-              <div className="actions flex flex-col justify-between items-start gap-3">
-                <div className="relative">
-                  <button
-                    className={cn(
-                      'flex items-center justify-center border-none outline-none hover:text-primary-400 cursor-pointer',
-                      isEmojiPickerOpen && 'text-primary-400'
-                    )}
-                    title="Add Emoji"
-                    onClick={() => setIsEmojiPickerOpen(!isEmojiPickerOpen)}>
-                    <LuSmile className="size-6" />
-                  </button>
-                  {isEmojiPickerOpen && (
-                    <Picker
-                      // className="sticky top-0 left-0"
-                      searchPlaceHolder={t('Search Emojis')}
-                      theme={theme === 'light' ? Theme.LIGHT : Theme.DARK}
-                      style={{ position: 'absolute', top: '100%', left: '0' }}
-                      onEmojiClick={(emojiData) => {
-                        // console.log('emojiData', emojiData);
-                        setPostContent(postContent + emojiData.emoji);
-                      }}
-                    />
-                  )}
-                </div>
-
-                <div className="additional__actions flex gap-4">
-                  <label className="hover:text-primary-400 cursor-pointer" title="Add Image">
-                    <LuImage className="size-6" />
-                    <input type="file" accept="image/*" className="hidden" multiple onChange={onPickImage} />
-                  </label>
-                  <label className="hover:text-primary-400 cursor-pointer" title="Add Video">
-                    <LuVideo className="size-6" />
-                    <input type="file" accept="video/*" className="hidden" multiple onChange={onPickVideo} />
-                  </label>
-                  <button className="hover:text-primary-400 cursor-pointer" title="Add Event">
-                    <LuCalendar className="size-6" />
-                  </button>
-                  <button className="hover:text-primary-400 cursor-pointer" title="Add Other ">
-                    <LuPlus className="size-6" />
-                  </button>
-                </div>
-                <div className="mt-4 flex justify-end items-center gap-2 w-full">
-                  <Button className="flex items-center gap-2 hover:text-primary-400 cursor-pointer">
-                    <LuClock className="size-6" />
-                  </Button>
-
-                  <Button
-                    className={cn(
-                      'inline-flex items-center gap-2 py-1.5 px-12 border-2  rounded-full text-sm/6 font-semibold  shadow-inner shadow-white/10 hover:bg-primary-400',
-                      'border-primary-400 cursor-pointer',
-                      'transition-all duration-300 ease-in-out'
-                    )}
-                    onClick={() => setIsModalOpen(false)}>
-                    Post
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </DialogPanel>
-        </div>
-      </div>
-    </Dialog>
   );
 };
 
