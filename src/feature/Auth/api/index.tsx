b2f846c41ff9e7e5b49bee06b74481361414aefb
Fabrice-Mokfembam <fabricemokfembam@gmail.com>
2025-01-25 12:45:51 +0200

    fix bugs on auth

--- src/feature/Auth/api/index.tsx
+++ src/feature/Auth/api/index.tsx
@@ -1,66 +1,80 @@
-import { jwtDecode } from 'jwt-decode';
-import { AUTH_TOKEN_KEY } from '../../../constants';
-import { CodeVerify, EmailCode, PhoneCode, PhoneVerify, User } from '../../../models/datamodels';
-import { apiClient } from '../../../services/apiClient';
+import { apiClient} from "../../../services/apiClient";
+import {
+  CodeVerify,
+  EmailCode,
+  PhoneCode,
+  PhoneVerify,
+  User,
+} from "../../../models/datamodels";
+import { jwtDecode } from "jwt-decode";
+import { ACCESS_TOKEN_KEY } from "../../../constants";
 
 // Register user
 const registerUser = async (user: User) => {
-  console.log('Registering user:', user);
-  const data = await apiClient.post('/auth/register', user);
+  console.log("second", user);
+  const { data } = await apiClient.post("/auth/register", user);
   return data;
 };
 
-// Update user
+// Register user
 const updateUser = async (user: User) => {
-  //Promise<User | undefined>
-  const token = localStorage.getItem(AUTH_TOKEN_KEY);
-  if (!token) {
-    return undefined;
+  const token = localStorage.getItem(ACCESS_TOKEN_KEY);
+
+  if (token) {
+    const tokenDecoded = jwtDecode(token);
+    console.log("second", tokenDecoded.sub, user);
+    const { data } = await apiClient.patch(
+      `/users/${tokenDecoded.sub}`,
+      user
+    );
+    return data;
   }
-  const tokenDecoded = jwtDecode(token);
-  console.log('Updating user:', tokenDecoded.sub, user);
-  const { data } = await apiClient.patch(`/users/${tokenDecoded.sub}`, user);
-  return data;
 };
 
 // Login user
 const loginUser = async (user: User) => {
-  console.log('Logging in user:', user);
-  const { data } = await apiClient.post('/auth/login-email', user);
+  console.log(user);
+  const { data } = await apiClient.post("/auth/login-email", user);
   return data;
 };
 
 // Get email verification code
 const getEmailVerificationCode = async (user: EmailCode) => {
   console.log(user);
-  const { data } = await apiClient.post('/auth/send-verification-email', user);
+  const { data } = await apiClient.post(
+    "/auth/send-verification-email",
+    user
+  );
   return data;
 };
 
 // Verify email code
 const verifyEmailCode = async (user: CodeVerify) => {
   console.log(user);
-  const { data } = await apiClient.post('/auth/verify-email', user);
+  const { data } = await apiClient.post("/auth/verify-email", user);
   return data;
 };
 
 // Get phone verification code
 const getPhoneVerificationCode = async (user: PhoneCode) => {
   console.log(user);
-  const { data } = await apiClient.post('/auth/send-verification-sms', user);
+  const { data } = await apiClient.post(
+    "/auth/send-verification-sms",
+    user
+  );
   return data;
 };
 
 // Verify phone code
 const verifyPhoneCode = async (user: PhoneVerify) => {
   console.log(user);
-  const { data } = await apiClient.post('/auth/verify-phone', user);
+  const { data } = await apiClient.post("/auth/verify-phone", user);
   return data;
 };
 
 // Logout user
 const logoutUser = async (refreshToken: string) => {
-  const { data } = await apiClient.post('/auth/logout', {
+  const { data } = await apiClient.post("/auth/logout", {
     refreshToken,
   });
   return data;
@@ -68,7 +82,7 @@ const logoutUser = async (refreshToken: string) => {
 
 // Refresh tokens
 const refreshAuthTokens = async (refreshToken: string) => {
-  const { data } = await apiClient.post('/auth/refresh-tokens', {
+  const { data } = await apiClient.post("/auth/refresh-tokens", {
     refreshToken,
   });
   return data;
@@ -76,7 +90,7 @@ const refreshAuthTokens = async (refreshToken: string) => {
 
 // Forgot password - send reset password email
 const forgotPassword = async (email: string) => {
-  const { data } = await apiClient.post('/auth/forgot-password', {
+  const { data } = await apiClient.post("/auth/forgot-password", {
     email,
   });
   return data;
@@ -84,27 +98,37 @@ const forgotPassword = async (email: string) => {
 
 // Verify reset password code
 const verifyResetPasswordCode = async (code: string, email: string) => {
-  const { data } = await apiClient.post('/auth/reset-password-code-verify', { code, email });
+  const { data } = await apiClient.post(
+    "/auth/reset-password-code-verify",
+    { code, email }
+  );
   return data;
 };
 
 // Reset password
-const resetPassword = async (token: string, email: string, password: string) => {
-  const { data } = await apiClient.post(`/auth/reset-password?token=${token}`, { email, password });
+const resetPassword = async (
+  token: string,
+  email: string,
+  password: string
+) => {
+  const { data } = await apiClient.post(
+    `/auth/reset-password?token=${token}`,
+    { email, password }
+  );
   return data;
 };
 
 export {
-  forgotPassword,
-  getEmailVerificationCode,
-  getPhoneVerificationCode,
+  registerUser,
   loginUser,
+  getEmailVerificationCode,
+  verifyEmailCode,
+  getPhoneVerificationCode,
+  verifyPhoneCode,
   logoutUser,
   refreshAuthTokens,
-  registerUser,
+  forgotPassword,
+  verifyResetPasswordCode,
   resetPassword,
   updateUser,
-  verifyEmailCode,
-  verifyPhoneCode,
-  verifyResetPasswordCode,
 };
