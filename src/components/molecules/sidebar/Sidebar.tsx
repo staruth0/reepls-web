d300e70aa1769adc08b035c73e7369168d56c1f1
glennfor <glennfor72@gmail.com>
2025-03-04 11:22:22 -0500

    fix: correct a few styling problems

--- src/components/molecules/sidebar/Sidebar.tsx
+++ src/components/molecules/sidebar/Sidebar.tsx
@@ -1,78 +1,77 @@
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 
 // import * as RadixPopover from '@radix-ui/react-popover';
-import React, { useContext, useState } from "react";
-import { FaRegUserCircle } from "react-icons/fa";
+import React, { useContext, useState } from 'react';
+import { FaRegUserCircle } from 'react-icons/fa';
 import {
   LuBell,
   LuBookmark,
-  LuCircleChevronLeft,
+  LuCircleChevronRight,
   LuCirclePlus,
   LuHouse,
   LuPencilLine,
   LuPlus,
   LuSearch,
-} from "react-icons/lu";
-import { useNavigate } from "react-router-dom";
-import SidebarItem from "../../atoms/SidebarItem";
+} from 'react-icons/lu';
+import { useNavigate } from 'react-router-dom';
+import SidebarItem from '../../atoms/SidebarItem';
 
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 // import { AuthContext } from '../../../context/AuthContext/authContext';
-import { SidebarContext } from "../../../context/SidebarContext/SidebarContext";
-import { cn } from "../../../utils";
-import PostModal from "../../../feature/Blog/components/PostModal";
-import "./sidebar.scss";
-import { reeplsIcon } from "../../../assets/icons";
-import { useCreateArticle } from "../../../feature/Blog/hooks/useArticleHook";
-import { Article } from "../../../models/datamodels";
-import { toast } from "react-toastify";
-import { Spinner } from "../../atoms/Spinner";
-import { useUser } from "../../../hooks/useUser";
-
+import { toast } from 'react-toastify';
+import { reeplsIcon } from '../../../assets/icons';
+import { SidebarContext } from '../../../context/SidebarContext/SidebarContext';
+import PostModal from '../../../feature/Blog/components/PostModal';
+import { useCreateArticle } from '../../../feature/Blog/hooks/useArticleHook';
+import { useUser } from '../../../hooks/useUser';
+import { Article } from '../../../models/datamodels';
+import { cn } from '../../../utils';
+import { Spinner } from '../../atoms/Spinner';
+import './sidebar.scss';
 
 const Sidebar: React.FC = () => {
   const navigate = useNavigate();
-  const {authUser} = useUser()
+  const { authUser } = useUser();
   const [isCreatingPost, setIsCreatingPost] = useState<boolean>(false);
   const { t } = useTranslation();
-  const { mutate ,isPending} = useCreateArticle();
+  const { mutate, isPending } = useCreateArticle();
   // const { isTablet } = useResponsiveLayout();
   const { isOpen, toggleSidebar } = useContext(SidebarContext);
   const handleToggleSidebar = () => {
-    console.log("Toggle sidebar", isOpen);
+    console.log('Toggle sidebar', isOpen);
     toggleSidebar();
   };
 
   const navLinks = [
     {
       icon: LuHouse,
-      name: "Feed",
-      link: "/feed",
+      name: 'Feed',
+      link: '/feed',
     },
     {
       icon: LuSearch, // or LuCompass
-      name: "Search", // or Explore
-      link: "/explore",
+      name: 'Search', // or Explore
+      link: '/explore',
     },
     {
       icon: LuBookmark,
-      name: "Bookmarks",
-      link: "/bookmarks",
+      name: 'Bookmarks',
+      link: '/bookmarks',
     },
     {
       icon: LuBell,
-      name: "Notifications",
-      link: "/notifications",
+      name: 'Notifications',
+      link: '/notifications',
       badgeContent: 14,
     },
     {
       icon: FaRegUserCircle,
-      name: "Profile",
+      name: 'Profile',
       link: `/profile/${authUser?.username}`,
     },
   ];
 
-  const handlePost = ( postContent: string,postImages: File[],postVideos: File[]) => {
+  const handlePost = (postContent: string, postImages: File[], postVideos: File[]) => {
     const post: Article = {
       content: postContent,
     };
@@ -80,48 +79,43 @@ const Sidebar: React.FC = () => {
     mutate(post, {
       onSuccess: () => {
         setIsCreatingPost(false);
-        console.log("Article created successfully");
-        toast.success("Article created successfully");
-        navigate("/feed")
+        console.log('Article created successfully');
+        toast.success('Article created successfully');
+        navigate('/feed');
       },
       onError: (error) => {
-        console.error("Error creating article", error);
-        toast.error("Error creating article"+ error);
+        console.error('Error creating article', error);
+        toast.error('Error creating article' + error);
       },
     });
 
-    console.log("post", post);
-        
-    console.log("postContent", postContent);
-    console.log("postImages", postImages);
-    console.log("postVideos", postVideos);
+    console.log('post', post);
+
+    console.log('postContent', postContent);
+    console.log('postImages', postImages);
+    console.log('postVideos', postVideos);
   };
 
   // fixed top-0 h-screen w-[65%] bg-white z-10
 
   return (
     <div className="side">
-      <LuCircleChevronLeft
+      <LuCircleChevronRight
         className={cn(
-          "size-6 p-0 rounded-full cursor-pointer",
-          isOpen && "rotate-180",
-          "transition-all duration-300 ease-in-out",
-          "hover:text-primary-400",
-          "bg-background border-none absolute z-10 top-1/2 right-0 transform translate-x-1/2 text-neutral-400"
+          'size-6 p-0 rounded-full cursor-pointer',
+          isOpen && 'rotate-180',
+          'transition-all duration-300 ease-in-out',
+          'hover:text-primary-400',
+          'bg-background border-none absolute z-10 top-1/2 right-0 transform translate-x-1/2 text-neutral-400'
         )}
         onClick={() => handleToggleSidebar()}
       />
       <div className="flex gap-5 items-center h-[80px]">
         <div
           className=" text-roboto text-[24px] font-semibold flex gap-2 items-center cursor-pointer"
-          onClick={() => navigate("/feed")}
-        >
-          <img
-            src={reeplsIcon}
-            alt="reeplsicon"
-            className={`${isOpen ? "size-8" : "size-9"}`}
-          />
-          {isOpen && "REEPLS"}
+          onClick={() => navigate('/feed')}>
+          <img src={reeplsIcon} alt="reeplsicon" className={`${isOpen ? 'size-8' : 'size-9'}`} />
+          {isOpen && 'REEPLS'}
         </div>
       </div>
 
@@ -141,57 +135,36 @@ const Sidebar: React.FC = () => {
       {isCreatingPost && (
         <>
           {isPending ? (
-            <Spinner/>
+            <Spinner />
           ) : (
-            <PostModal
-              isModalOpen={isCreatingPost}
-              setIsModalOpen={setIsCreatingPost}
-              handlePost={handlePost}
-            />
+            <PostModal isModalOpen={isCreatingPost} setIsModalOpen={setIsCreatingPost} handlePost={handlePost} />
           )}
         </>
       )}
       <div className="create__post__btn">
         <Popover className="relative">
           <PopoverButton
-            className={cn(
-              `create__post__button `,
-              "disabled:text-neutral-400 disabled:cursor-not-allowed"
-            )}
-            disabled={isCreatingPost}
-          >
-            <LuCirclePlus
-              className="create__post__icon"
-              style={{ width: "20px", height: "20px" }}
-            />
+            className={cn(`create__post__button `, 'disabled:text-neutral-400 disabled:cursor-not-allowed')}
+            disabled={isCreatingPost}>
+            <LuCirclePlus className="create__post__icon" style={{ width: '20px', height: '20px' }} />
             {isOpen && t(`Create Post`)}
           </PopoverButton>
           <PopoverPanel
             anchor="bottom"
-            className={cn(
-              "PopoverContent flex flex-col bg-red-500 z-50 mt-2",
-              !isOpen ? "w-32" : "w-40"
-            )}
-          >
+            className={cn('PopoverContent flex flex-col bg-red-500 z-50 mt-2', !isOpen ? 'w-32' : 'w-40')}>
             <div className="block text-center">
               <button
                 className="flex items-center justify-center gap-2 cursor-pointer py-3 px-4 hover:text-primary-400"
-                onClick={() => setIsCreatingPost(true)}
-              >
+                onClick={() => setIsCreatingPost(true)}>
                 <LuPlus className="size-4" />
-                <span className="text-sm">
-                  {!isOpen ? t(`Post`) : t(`Create Post`)}
-                </span>
+                <span className="text-sm">{!isOpen ? t(`Post`) : t(`Create Post`)}</span>
               </button>
               <hr className="border-neutral-400 w-3/4 mx-auto" />
               <button
                 className="flex items-center justify-center gap-2 cursor-pointer py-3 px-4  hover:text-primary-400"
-                onClick={() => navigate("/posts/create")}
-              >
+                onClick={() => navigate('/posts/create')}>
                 <LuPencilLine className="size-4" />
-                <span className="text-sm">
-                  {!isOpen ? t(`Write`) : t(`Write Article`)}
-                </span>
+                <span className="text-sm">{!isOpen ? t(`Write`) : t(`Write Article`)}</span>
               </button>
             </div>
           </PopoverPanel>
--- src/feature/Search/components/SearchTopBar.tsx
+++ src/feature/Search/components/SearchTopBar.tsx
@@ -1,54 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { LuSearch, LuX } from "react-icons/lu";
-import SearchContainer from "./SearchContainer";
-import { useNavigate } from "react-router-dom";
-import { useGetSearchSuggestions } from "../hooks";
-import { useUser } from "../../../hooks/useUser";
-
-
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { LuSearch, LuX } from 'react-icons/lu';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../../../hooks/useUser';
+import { useGetSearchSuggestions } from '../hooks';
+import SearchContainer from './SearchContainer';
 
 const SearchTopBar: React.FC = () => {
   const { t } = useTranslation();
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>('');
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const {authUser} = useUser()
+  const { authUser } = useUser();
 
-  const {data} = useGetSearchSuggestions(authUser?.id || "")
-
+  const { data } = useGetSearchSuggestions(authUser?.id || '');
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
   const handleClearSearch = () => {
-    setSearchTerm("");
+    setSearchTerm('');
     setIsOpen(false);
   };
 
   const handleSearch = () => {
     const trimmedSearchTerm = searchTerm.trim();
     if (trimmedSearchTerm) {
-      navigate(`/search/results/${trimmedSearchTerm.replace(/ /g, "+")}`);
-   
+      navigate(`/search/results/${trimmedSearchTerm.replace(/ /g, '+')}`);
     }
     setIsOpen(false);
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       handleSearch();
     }
   };
 
-
   useEffect(() => {
-    if (data) { 
-      console.log(data.searchHistory)
+    if (data) {
+      console.log(data.searchHistory);
     }
-  }, [data])
-  
+  }, [data]);
+
   useEffect(() => {
     if (searchTerm) {
       setIsOpen(true);
@@ -64,10 +59,10 @@ const SearchTopBar: React.FC = () => {
         value={searchTerm}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
-        placeholder={t("Search")}
+        placeholder={t('Search')}
         className="w-full px-4 py-3 border-none bg-neutral-600 rounded-full outline-none"
       />
-      <div className="absolute top-3 right-5 cursor-pointer flex items-center gap-1">
+      <div className="absolute top-3 right-5 cursor-pointer flex items-center gap-1 text-neutral-400">
         {searchTerm && <LuX className="size-5" onClick={handleClearSearch} />}
         <LuSearch className="size-6" onClick={handleSearch} />
       </div>
