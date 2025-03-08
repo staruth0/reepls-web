f9bef63e14f0ba29264b135c467b8ca2efa81683
glennfor <glennfor72@gmail.com>
2025-03-08 10:43:47 -0500

    fix: cleanup swiper

--- src/components/atoms/TopRightComponent.tsx
+++ src/components/atoms/TopRightComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import {  LuStar } from 'react-icons/lu';
 
+import { commuLeft } from '../../assets/icons';
 import './index.scss';
 
 const TopRightComponent: React.FC = () => {
@@ -9,11 +9,10 @@ const TopRightComponent: React.FC = () => {
   return (
     <div className="right__top__bar">
       <div>
-        {/* <img src={commuLeft} alt="star" /> */}
-        <LuStar className="size-6 bg-main-yellow rounded-full p-1" strokeWidth={2.5} />
-      <div>{t(`Communiques`)}</div>
+        <img src={commuLeft} alt="star" />
+        {/* <LuStar className="size-6 bg-main-yellow rounded-full p-1" strokeWidth={2.5} /> */}
+        <div>{t(`Communiques`)}</div>
       </div>
-     
     </div>
   );
 };
